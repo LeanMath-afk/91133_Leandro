0f32155080e602d0cffa81bb379c2650f6db96e0
LeandroMath-afk <leandrosilvajesus2325@gmail.com>
2025-05-14 10:09:01 -0300

    Add files via upload

---
+++ HTMLeandro/SISTEMA/static/script.js
@@ -0,0 +1,34 @@
+function adicionaramigo(){
+    const nome = document.getElementById('nome').value;
+    if(nome === '') return;
+
+    fetch('/api/adicionar', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ nome: nome})
+    })
+    .then(res => res.json())
+    .then(() => {
+        document.getElementById('nome').value = '';
+        carregarLista();
+    });
+}
+
+function carregarLista(){
+    fetch('/api/listar')
+    .then(res => res.json())
+    .then(amigos => {
+        const lista = document.getElementById('lista-amigos').value = '';
+        lista.innerHTML = '';
+        amigos.forEach(nome => {
+            const item = document.createElement('li');
+            item.textContent = nome;
+            lista.appendChild(item);
+        });
+    });
+}
+
+// carregar a lista quando a página abrir 
+window.onload =  carregarLista;
